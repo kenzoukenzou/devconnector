3de35c6190ed7205976d25a0e0fe3692b7757a04
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-05-28 16:32:02 +0900

    Alert compo & call it

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // redux
 import { Provider } from "react-redux";
@@ -17,6 +19,7 @@ const App = () => {
           <Navbar />
           <Route exact path="/" component={Landing} />
           <section className="container">
+            <Alert />
             <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
